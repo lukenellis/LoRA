7c54ff06fca7ec4642e6f67ca4dae9b1c15c336c
lukenellis <131164727+lukenellis@users.noreply.github.com>
2025-04-30 16:22:33 -0400

    Update run_glue.py

--- examples/NLU/examples/text-classification/run_glue.py
+++ examples/NLU/examples/text-classification/run_glue.py
@@ -515,13 +515,15 @@ def main():
     # You can define your custom compute_metrics function. It takes an `EvalPrediction` object (a namedtuple with a
     # predictions and label_ids field) and has to return a dictionary string to float.
     def compute_metrics(p: EvalPrediction):
+        preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+        preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
+
         print(">> compute_metrics debug:")
         print("Preds:", preds[:10])
         print("Labels:", p.label_ids[:10])
         print("Shape preds:", preds.shape)
         print("Shape labels:", p.label_ids.shape)
-        preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
-        preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
+        
         if data_args.task_name is not None:
             result = metric.compute(predictions=preds, references=p.label_ids)
             if len(result) > 1:
