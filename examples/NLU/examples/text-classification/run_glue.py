91ee0fd756371292228a9c7dd1701f2f938ee430
Armaity Khurshed Katki <131193721+armaitykk@users.noreply.github.com>
2025-05-01 00:01:06 -0400

    Update run_glue.py

--- examples/NLU/examples/text-classification/run_glue.py
+++ examples/NLU/examples/text-classification/run_glue.py
@@ -513,23 +513,6 @@ def main():
     if data_args.task_name is not None:
         metric = load_metric("glue", data_args.task_name)
 
-    # TODO: When datasets metrics include regular accuracy, make an else here and remove special branch from
-    # compute_metrics
-
-     # You can define your custom compute_metrics function. It takes an `EvalPrediction` object (a namedtuple with a
-     # predictions and label_ids field) and has to return a dictionary string to float.
-    def compute_metrics(p: EvalPrediction):
-        preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
-        preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
-        if data_args.task_name is not None:
-            result = metric.compute(predictions=preds, references=p.label_ids)
-            if len(result) > 1:
-                result["combined_score"] = np.mean(list(result.values())).item()
-            return result
-        elif is_regression:
-            return {"mse": ((preds - p.label_ids) ** 2).mean().item()}
-        else:
-            return {"accuracy": (preds == p.label_ids).astype(np.float32).mean().item()}
 
     # Data collator will default to DataCollatorWithPadding, so we change it if we already did the padding.
     if data_args.pad_to_max_length:
@@ -666,6 +649,19 @@ def main():
     plt.ylabel('Seconds')
     plt.savefig("inference_timing_chart.png")
     plt.show()
+
+def compute_metrics(p: EvalPrediction):
+        preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+        preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
+        if data_args.task_name is not None:
+            result = metric.compute(predictions=preds, references=p.label_ids)
+            if len(result) > 1:
+                result["combined_score"] = np.mean(list(result.values())).item()
+            return result
+        elif is_regression:
+            return {"mse": ((preds - p.label_ids) ** 2).mean().item()}
+        else:
+            return {"accuracy": (preds == p.label_ids).astype(np.float32).mean().item()}
 
 # === LoRA Rank Search with dynamic r ===
 def search_lora_rank(model_args, data_args, training_args, tokenizer, num_labels, datasets, is_regression, metric, config, train_dataset, eval_dataset, data_collator):
